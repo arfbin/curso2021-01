d8ad5f2e29542a808c83c8c0c36d1b23ebf70502
arfbin <arodriguez@binovo.es>
2021-04-13 15:42:28 +0000

    [ADD] New module helpdesk_arodriguez

---
+++ helpdesk_arodriguez/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 AEODOO
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Helpdesk Alicia Rodriguez",
+    "version": "14.0.1.0.0",
+    "author": "Alicia Rodríguez",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/helpdesk_menu.xml"
+    ],    
+}
