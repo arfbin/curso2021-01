ccf82da3cac709ef744826b853b50aef4fb26ec2
arfbin <arodriguez@binovo.es>
2021-04-20 16:42:34 +0000

    [ADD] Add new models relations and compute fields

--- helpdesk_arodriguez/models/helpdesk_ticket.py
+++ helpdesk_arodriguez/models/helpdesk_ticket.py
@@ -1,4 +1,28 @@
-from odoo import models, fields
+from odoo import api, models, fields
+
+
+class HelpdeskTicketAction(models.Model):
+    _name = 'helpdesk.ticket.action'
+    _description = 'Ticket Actions'
+
+    name = fields.Char(required=True)
+    date = fields.Date()
+    ticket_id = fields.Many2one(
+        comodel_name='helpdesk.ticket', 
+        string='Ticket')
+
+
+class HelpdeskTicketTag(models.Model):
+    _name = 'helpdesk.ticket.tag'
+    _description = 'Ticket Tags'
+
+    name = fields.Char(
+        string='Name', 
+        required=True)
+    ticket_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket', 
+        string='Tickets')
+    
 
 class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
@@ -24,7 +48,7 @@ class HelpdeskTicket(models.Model):
         string='Time')
     assigned = fields.Boolean(
         string='Assigned',
-        readonly=True)
+        compute='_compute_assigner')
     deadline = fields.Date(
         string='Deadline')
     action_corrective = fields.Html(
@@ -33,6 +57,21 @@ class HelpdeskTicket(models.Model):
     action_preventive = fields.Html(
         string='Preventive Action',
         help='Descrive preventive actions to do')
+    user_id = fields.Many2one(
+        comodel_name='res.users', 
+        string='Assign to')
+    action_ids = fields.One2many(
+        comodel_name='helpdesk.ticket.action',
+        inverse_name='ticket_id',
+        string='Actions')
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket.tag',
+        string='Tags')
+    ticket_qty = fields.Integer(
+        string='Ticket Qty', 
+        compute='_compute_ticket_qty')
+    tag_name = fields.Char(
+        string='Tag Name')
     
     #Asignar, cambia estado a asignado y pone a true el campo asignado, visible sólo con estado = nuevo
     def do_assign(self):
@@ -54,3 +93,19 @@ class HelpdeskTicket(models.Model):
         self.ensure_one()
         self.state = 'canceled'
         
+    @api.depends('user_id')
+    def _compute_assigner(self):
+        for record in self:
+            record.assigned = self.user_id and True or False
+
+    @api.depends('user_id')
+    def _compute_ticket_qty(self):
+        for record in self:
+            other_tickets = self.env['helpdesk.ticket'].search([('user_id', '=', record.user_id.id)])
+            record.ticket_qty = len(other_tickets)
+    
+    def create_tag(self):
+        self.ensure_one()
+        self.write({
+            'tag_ids': [(0,0,{'name': self.tag_name})]
+        })
